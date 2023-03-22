3241493366c05877bb80cae5556fe3694cb5d66b
jiajing-2000 <candymagic2000@qq.com>
2023-03-22 04:50:24 -0400

    part1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,39 @@
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.number}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <Content name={course.parts[0].name} number={course.parts[0].exercises} />
+      <Content name={course.parts[1].name} number={course.parts[1].exercises} />
+      <Content name={course.parts[2].name} number={course.parts[2].exercises} />
+      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const StatisticLine = (props) => {
+  return (
+      <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+      </tr>
+  );
+};
+
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  const average = ((props.good - props.bad) / total).toFixed(1)
+  const positive = (props.good / total * 100).toFixed(1)
+
+  if (total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    </div>
+  )
+
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
